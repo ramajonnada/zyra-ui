589373cc51c3dc67c38b100cae5c886d4627dd11
ramajonnada <rama.jonnada23@gmail.com>
2025-11-17 06:48:06 +0530

    create basic componets, pages and services for basic blog website

--- projects/zyra-ui/src/app/app.routes.ts
+++ projects/zyra-ui/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { BlogPost } from './pages/blog-post/blog-post';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	// {
+	// 	path: 'blog/:slug',
+	// 	loadComponent: () =>
+	// 		import('./pages/blog-post/blog-post').then(m => m.BlogPost)
+	// },
+	{ path: '', component: Home },
+	{ path: 'about', component: About },
+	{ path: 'blog-post', component: BlogPost },
+
+];
---
+++ projects/zyra-ui/src/app/pages/blog-post/blog-post.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post',
+  imports: [],
+  templateUrl: './blog-post.html',
+  styleUrl: './blog-post.scss',
+})
+export class BlogPost {
+
+}
