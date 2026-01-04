bc746e59ce9d547115d92b45dd5c48988953aae8
ramajonnada <rama.jonnada23@gmail.com>
2026-01-04 10:41:30 +0530

    feat(header): add zyra-ng-ui button

--- projects/zyra-ui/src/app/components/header/header.ts
+++ projects/zyra-ui/src/app/components/header/header.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Button } from 'zyra-ui-lib';
+import { Button } from 'zyra-ng-ui';
 
 @Component({
 	selector: 'app-header',
