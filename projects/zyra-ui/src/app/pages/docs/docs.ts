3a3542270d8516b0f6c44307fa19d07545f98278
ramajonnada <rama.jonnada23@gmail.com>
2026-01-06 14:49:55 +0530

    feat(contact): add templates and styles for contact page

--- projects/zyra-ui/src/app/pages/docs/docs.ts
+++ projects/zyra-ui/src/app/pages/docs/docs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Home } from "../home/home";
 
 @Component({
     selector: 'app-docs',
-    imports: [],
+    imports: [Home],
     templateUrl: './docs.html',
     styleUrl: './docs.scss',
 })
