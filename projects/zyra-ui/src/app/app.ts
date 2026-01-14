216d45abe25f2b647d70fce7c1d6e7e551e9f695
ramajonnada <rama.jonnada23@gmail.com>
2026-01-14 14:17:57 +0530

    feat(app, library): theme setup for app using library

--- projects/zyra-ui/src/app/app.ts
+++ projects/zyra-ui/src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
-import { ZyraButton, ThemeService } from 'zyra-ng-ui';
+import { ThemeService } from 'zyra-ng-ui';
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterModule, Header, Footer, ZyraButton],
+	imports: [RouterModule, Header, Footer],
 	templateUrl: './app.html',
 	styleUrl: './app.scss',
 })
--- projects/zyra-ui/src/app/components/header/header.ts
+++ projects/zyra-ui/src/app/components/header/header.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ThemeService, ZyraButton, ZyraTheme } from 'zyra-ng-ui';
 
 @Component({
 	selector: 'app-header',
-	imports: [RouterLink],
+	imports: [RouterLink, ZyraButton],
 	templateUrl: './header.html',
 	styleUrl: './header.scss',
 })
-export class Header { }
+export class Header {
+
+
+	constructor(private _themeService: ThemeService) {
+
+	}
+	$toggleTheme(themeType: ZyraTheme) {
+		this._themeService.toggleTheme();
+
+	}
+}
