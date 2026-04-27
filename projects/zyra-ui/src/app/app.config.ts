01d5976a92e6ab32addd09fb1f0b241002b33a63
ramajonnada <rama.jonnada23@gmail.com>
2026-04-27 18:06:50 +0530

    feat(app, footer): add scrolling to top when ever user change from one route to another

--- projects/zyra-ui/src/app/app.config.ts
+++ projects/zyra-ui/src/app/app.config.ts
@@ -4,7 +4,7 @@ import {
     provideBrowserGlobalErrorListeners,
     provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -16,7 +16,12 @@ export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideZonelessChangeDetection(),
-        provideRouter(routes),
+        provideRouter(
+            routes,
+            withInMemoryScrolling({
+                scrollPositionRestoration: 'top',
+            }),
+        ),
         provideClientHydration(withEventReplay()),
         provideHttpClient(),
         importProvidersFrom(MarkdownModule.forRoot()),
