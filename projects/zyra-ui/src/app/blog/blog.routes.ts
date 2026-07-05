9bae31b61d5f79c7465c992fdae0ca5233f69c9b
ramajonnada <rama.jonnada23@gmail.com>
2026-07-05 16:32:17 +0530

    chore(zyra-ui): remove highlight.js sysntax highlighting from blog

--- projects/zyra-ui/src/app/blog/blog.routes.ts
+++ projects/zyra-ui/src/app/blog/blog.routes.ts
@@ -1,37 +1,11 @@
 import { Routes } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
-import { MarkdownModule, MARKED_EXTENSIONS } from 'ngx-markdown';
-import { markedHighlight } from 'marked-highlight';
-import hljs from 'highlight.js/lib/core';
-import typescript from 'highlight.js/lib/languages/typescript';
-import xml from 'highlight.js/lib/languages/xml';
-import scss from 'highlight.js/lib/languages/scss';
-import json from 'highlight.js/lib/languages/json';
-import bash from 'highlight.js/lib/languages/bash';
-
-hljs.registerLanguage('typescript', typescript);
-hljs.registerLanguage('html', xml);
-hljs.registerLanguage('scss', scss);
-hljs.registerLanguage('json', json);
-hljs.registerLanguage('bash', bash);
+import { MarkdownModule } from 'ngx-markdown';
 
 export const BLOG_ROUTES: Routes = [
     {
         path: '',
-        providers: [
-            importProvidersFrom(MarkdownModule.forRoot()),
-            {
-                provide: MARKED_EXTENSIONS,
-                useValue: markedHighlight({
-                    langPrefix: 'hljs language-',
-                    highlight(code, lang) {
-                        const language = hljs.getLanguage(lang) ? lang : 'typescript';
-                        return hljs.highlight(code, { language }).value;
-                    },
-                }),
-                multi: true,
-            },
-        ],
+        providers: [importProvidersFrom(MarkdownModule.forRoot())],
         children: [
             {
                 path: '',
